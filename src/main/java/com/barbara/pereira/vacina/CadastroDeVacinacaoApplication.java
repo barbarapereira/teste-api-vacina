d3d1e381e78404c9ab1300bd29270cc2a0a8e644
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-03-03 20:58:36 -0300

    criando o projeto

---
+++ src/main/java/com/barbara/pereira/vacina/CadastroDeVacinacaoApplication.java
@@ -0,0 +1,13 @@
+package com.barbara.pereira.vacina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeVacinacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeVacinacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbara/pereira/vacina/CadastroDeVacinacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbara.pereira.vacina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeVacinacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
